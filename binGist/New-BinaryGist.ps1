a60f0ffae28f806233c60302e214ea63f5ab95e8
TurboBasic <off@boun.cr>
2018-01-02 09:51:05 +0200

    Fixes in code

--- binGist/New-BinaryGist.ps1
+++ binGist/New-BinaryGist.ps1
@@ -1,16 +1,16 @@
 function New-BinaryGist {
 <#
     .SYNOPSIS
-Creates binary gists on GitHub. 
+Creates binary gists on GitHub.
 
     .DESCRIPTION
-Creates binary gists on GitHub.  Standard API doesn't allow this forcing users to 
+Creates binary gists on GitHub.  Standard API doesn't allow this forcing users to
 cumbersome manual operations, eg. https://gist.github.com/remarkablemark/feff40b0a522f0c41c4eff0b77ea1d47
 
     .EXAMPLE
 New-BinaryGist -path pirate_map.png -description 'Secret Map'
 
-#> 
+#>
 
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
     Param(
@@ -23,14 +23,14 @@ New-BinaryGist -path pirate_map.png -description 'Secret Map'
           $True
       })]
       [string[]]  $Path,
-      
+
       [Parameter(Position=1)]
-      [string]    $Description = "binGist: pirate's secret map",
-      
+      [string]    $Description = "binGist: pirate`'s secret map",
+
       [switch]    $Force
     )
-    
-  BEGIN {    
+
+  BEGIN {
     if (-not $psBoundParameters.ContainsKey('Verbose')) {
       $VerbosePreference = $psCmdlet.SessionState.psVariable.GetValue('VerbosePreference')
     }
@@ -41,14 +41,14 @@ New-BinaryGist -path pirate_map.png -description 'Secret Map'
     if (-not $psBoundParameters.ContainsKey('WhatIf')) {
       $WhatIfPreference =  $psCmdlet.SessionState.psVariable.GetValue('WhatIfPreference')
     }
-    $psBoundParameters.Remove('Force') | Out-Null 
+    $psBoundParameters.Remove('Force') | Out-Null
     $psBoundParameters.Remove('Path') | Out-Null
     $psBoundParameters.Remove('Description') | Out-Null
-    
-    Write-Verbose ('[{0}] Confirm={1} ConfirmPreference={2} WhatIf={3} WhatIfPreference={4}' -f 
+
+    Write-Verbose ('[{0}] Confirm={1} ConfirmPreference={2} WhatIf={3} WhatIfPreference={4}' -f
         $MyInvocation.MyCommand, $Confirm, $ConfirmPreference, $WhatIf, $WhatIfPreference
     )
-    
+
     # Text placeholder to start something with
     $placeholder = [IO.Path]::GetTempFileName()
     Write-Verbose "[$($MyInvocation.MyCommand)] Create temp placeholder $placeholder"
@@ -62,52 +62,52 @@ New-BinaryGist -path pirate_map.png -description 'Secret Map'
       Write-Error "[$($MyInvocation.MyCommand)] Cannot create placeholder file"
     }
   }
-  
+
   PROCESS {
-    if (-not $Force -and 
+    if (-not $Force -and
         -not $psCmdlet.ShouldProcess($Path -join ', ', "Create a gist based on set of files")
     ) {
       return
     }
     $ConfirmPreference = 'None'
-    
+
     # Create a gist with placeholder file with a text content as required by Github API
     try {
       $gist = New-Gist -path $placeholder -description $Description
     } catch {
       Write-Error "[$($MyInvocation.MyCommand)] Cannot create gist"
     } finally {
-      Remove-Item $placeholder -force 
+      Remove-Item $placeholder -force
     }
-    
-    # Fetch created gist from Github and remove placeholder 
+
+    # Fetch created gist from Github and remove placeholder
     git clone $gist.htmlUrl
     Set-Location $gist.id
     Remove-Item -path [IO.Path]::GetFileName($placeholder) @psBoundParameters
-      
+
     # Bring 'real' stuff to gist
-    foreach ($1path in $Path) 
-    {        
+    foreach ($1path in $Path)
+    {
       if (Test-Path $1path -pathType Leaf) {
-        Copy-Item -path $1path -destination . @psBoundParameters       
+        Copy-Item -path $1path -destination . @psBoundParameters
       } else {
         Write-Warning "[$($MyInvocation.MyCommand)] Skip $1path from copying to gist: item does not exist or is a directory"
       }
     }
-      
+
     # Commit the changes and push gist
     git add . > $Null
-    git commit -m "binGist: pirate's map replaced with payload"
+    git commit -m "binGist: pirate`'s map replaced with payload"
     Write-Verbose "[$($MyInvocation.MyCommand)] Git successfully committed the update gist"
-    
+
     try     { git push }
     catch   { "[$($MyInvocation.MyCommand)] Git cannot push repo to Github. Check local repo at ./$(Get-Location)" }
     finally { Set-Location .. }
-    
+
     # Get updated gist and return it to the user
-    Get-Gist -id $gist.id      
+    Get-Gist -id $gist.id
   }
-  
+
   END {}
-  
+
 }
